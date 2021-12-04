5d9a09d404759f02346ced21c49ac292f0a1b867
nurluorbit <57810104+nurluorbit@users.noreply.github.com>
2021-12-04 20:51:39 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# aircraft_sync_status
-Aircraft &amp; Sync Status Monitoring Automation Script
+# AC/Sync-Automation
+
+> The project was developed to scrape the data from Grafana.
+
+## How to Use
+### Step-1
+- **Download the repository**<br />
+### Step-2
+- **Configure the file paths which are listed below with comment lines.** <br />
+> Driver path defined --> Specify the driver path<br />
+### Step-3
+- **Type you username & password**<br />
+> Username and password entered--> Specify your username and password
---
+++ sync.py
@@ -0,0 +1,138 @@
+from selenium import webdriver
+from pyvirtualdisplay import Display
+import pandas as pd
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from pyvirtualdisplay import Display
+from bs4 import BeautifulSoup
+from datetime import date
+import json
+pd.set_option('display.max_rows', None)
+
+f = open("./BAU.json",)
+data = json.load(f) 
+
+#write_to_file(_date, datas)
+def write_to_file(_date, flights):
+    filename = '/tmp/sync-{}.csv'.format(_date)
+
+    with open(filename, 'a') as f:
+        for i in data["bau"]:
+            for j in datas:
+                if(j == i):
+                    f.write(f"{j} : {datas[j]} \n")
+    return
+
+_date = f"{date.today()}"
+
+driver = webdriver.Chrome(executable_path="~/Desktop/CODE/ac_sync/chromedriver")
+
+driver.get("Grafana Link")
+print("Login page is opened")
+driver.maximize_window()
+
+username = driver.find_element_by_xpath("/html/body/div/div/div[2]/div[3]/div/div[2]/div/div/form/div[1]/div[2]/div/div/input")
+password = driver.find_element_by_xpath("/html/body/div/div/div[2]/div[3]/div/div[2]/div/div/form/div[2]/div[2]/div/div/input")
+
+username.send_keys("E-Mail")
+password.send_keys("Password")
+
+driver.find_element_by_xpath("/html/body/div/div/div[2]/div[3]/div/div[2]/div/div/form/button").click()
+time.sleep(3)
+
+driver.get("Grafana Link 1")
+time.sleep(15)
+content = driver.page_source
+soup = BeautifulSoup(content)
+titles = []
+values = []
+datas = {}
+list1 = soup.findAll('span', attrs={'class':'panel-title-text'})
+list2 = soup.findAll('div', attrs={'class':'panel-content panel-content--no-padding'})
+
+for i in list1:
+    titles.append(f"{i.text}")
+for j in list2:
+    values.append(f'{j.div.div.div.div.div.div.contents[0].text}{j.div.div.div.div.div.div.contents[1].text}' if len(j.div.div.div.div.div.div.contents) == 2 else f'{j.div.div.div.div.div.div.contents[0].text}')
+for k in range(0, len(list1)):
+    v = None
+    for i in data["bau"]:
+        if (titles[k] == i):
+            v = True 
+        if (v == True):
+            break
+    if(v == None):
+        continue
+    if(values[k] == "No data"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    elif(values[k].split(" ")[0] == "OFF"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+    elif((float(values[k].split(" ")[0]) >= 1 and values[k].split(" ")[1] == "day") or (float(values[k].split(" ")[0]) >= 1.0 and values[k].split(" ")[1] == "week")):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    else:
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+print("---------")
+driver.get("Grafana Link 2")
+time.sleep(15)
+content = driver.page_source
+soup = BeautifulSoup(content)
+titles = []
+values = []
+list1 = soup.findAll('span', attrs={'class':'panel-title-text'})
+for i in list1:
+    titles.append(f"{i.text}")
+list2 = soup.findAll('div', attrs={'class':'panel-content panel-content--no-padding'}) 
+for j in list2:
+    values.append(f'{j.div.div.div.div.div.div.contents[0].text}{j.div.div.div.div.div.div.contents[1].text}' if len(j.div.div.div.div.div.div.contents) == 2 else f'{j.div.div.div.div.div.div.contents[0].text}')
+for k in range(0, len(list1)):
+    v = None
+    for i in data["bau"]:
+        if (titles[k] == i):
+            v = True 
+        if (v == True):
+            break
+    if(v == None):
+        continue
+    if(values[k] == "No data"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    elif(values[k].split(" ")[0] == "OFF"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+    elif((float(values[k].split(" ")[0]) >= 1 and values[k].split(" ")[1] == "day") or (float(values[k].split(" ")[0]) >= 1.0 and values[k].split(" ")[1] == "week")):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    else:
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+print("---------")
+driver.get("Grafana Link 3")
+time.sleep(15)
+content = driver.page_source
+soup = BeautifulSoup(content)
+titles = []
+values = []
+list1 = soup.findAll('span', attrs={'class':'panel-title-text'})
+for i in list1:
+    titles.append(f"{i.text}")
+list2 = soup.findAll('div', attrs={'class':'panel-content panel-content--no-padding'}) 
+for j in list2:
+    values.append(f'{j.div.div.div.div.div.div.contents[0].text}{j.div.div.div.div.div.div.contents[1].text}' if len(j.div.div.div.div.div.div.contents) == 2 else f'{j.div.div.div.div.div.div.contents[0].text}')
+for k in range(0, len(list1)):
+    v = None
+    for i in data["bau"]:
+        if (titles[k] == i):
+            v = True 
+        if (v == True):
+            break
+    if(v == None):
+        continue
+    if(values[k] == "No data"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    elif(values[k].split(" ")[0] == "OFF"):
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+    elif((float(values[k].split(" ")[0]) >= 1 and values[k].split(" ")[1] == "day") or (float(values[k].split(" ")[0]) >= 1.0 and values[k].split(" ")[1] == "week")):
+        datas.update({f"{titles[k]}" : f"{values[k]} - Check"})
+    else:
+        datas.update({f"{titles[k]}" : f"{values[k]} - OK"})
+write_to_file(_date, datas)
+driver.close()
